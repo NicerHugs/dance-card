59e453f4f0cf09e52d2638959f10127e93d597c5
Jess Scheuring <jscheuring@gmail.com>
2014-11-05 18:07:35 -0500

    removes unnecessary view

--- app/scripts/views/orgs/org/_events.js
+++
@@ -1,8 +0,0 @@
-DanceCard.Views.EventPartial = DanceCard.Views.Base.extend({
-  tagName: 'article',
-  className: 'org-event',
-  template: DanceCard.templates.orgs.org._event,
-  render: function() {
-    this.$el.html(this.template(this.model));
-  }
-});
