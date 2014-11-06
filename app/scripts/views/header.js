b8154817f8a3dcf213acac1094910a4cddb3c229
Jess Scheuring <jscheuring@gmail.com>
2014-11-06 09:36:05 -0500

    begins stying header

--- app/scripts/views/header.js
+++ app/scripts/views/header.js
@@ -3,6 +3,7 @@
     tagName: 'header',
     render: function() {
       this.$el.append('<h1><a href="#">Contra Dance Card</a></h1>');
+      this.$el.append('<span>Do you want to dance?</span>');
       this.navView = new DanceCard.Views.Nav({
         $container: this.$el,
         model: DanceCard.session
