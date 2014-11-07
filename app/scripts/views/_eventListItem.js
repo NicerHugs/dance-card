62b3e8da7f169a505a7e750b6887b37e13b659eb
Jess Scheuring <jscheuring@gmail.com>
2014-11-06 20:25:02 -0500

    adds rsvp button to search list results

--- app/scripts/views/_eventListItem.js
+++ app/scripts/views/_eventListItem.js
@@ -6,8 +6,18 @@
     className: 'search-result',
     template: DanceCard.templates._eventListItem,
     render: function() {
-      this.$el.html(this.template(this.model.toJSON()));
+      this.templateData = {
+        event: this.model.toJSON()};
+      if (DanceCard.session.get('user')) {
+        this.templateData.loggedIn = true;
+        this.templateData.dancer = (!DanceCard.session.get('user').organizer);
+        this.templateData.owner = (this.model.get('orgUrlId') === DanceCard.session.get('user').urlId);
+      } else {
+      this.templateData.loggedIn = false;
+      }
+      this.$el.html(this.template(this.templateData));
     }
+    
   });
 
 })();
