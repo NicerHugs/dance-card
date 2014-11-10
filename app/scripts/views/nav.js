f7a301486e74f500d39a1ae81d077be2b2f3e33b
Jess Scheuring <jscheuring@gmail.com>
2014-11-10 13:17:42 -0500

    fixes bug on logout w/nav display update

--- app/scripts/views/nav.js
+++ app/scripts/views/nav.js
@@ -21,6 +21,7 @@
     logout: function(e) {
       e.preventDefault();
       Parse.User.logOut();
+      DanceCard.session.set('user', Parse.User.current());
       DanceCard.router.navigate('login', {trigger: true});
       $('main').prepend('<div class="logout-msg">You have successfully logged out</div>');
       window.setTimeout(function() {
