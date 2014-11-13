15a927c27daa4e8ff38d085114282d0e7b7c6e85
Jess Scheuring <jscheuring@gmail.com>
2014-11-13 14:49:22 -0500

    fixes bug in dance card link

--- app/scripts/views/header.js
+++ app/scripts/views/header.js
@@ -8,7 +8,7 @@
       if (Parse.User.current()) {
         urlString = '/dancers/' + Parse.User.current().get('urlId');
       }
-      this.$el.append('<h1><a href=#"'+urlString+'">Dance Card</a></h1>');
+      this.$el.append('<h1><a href="#'+urlString+'">Dance Card</a></h1>');
       this.$el.append('<span class="tag-line">Do you want to dance?</span>');
       this.navView = new DanceCard.Views.Nav({
         $container: this.$el,
