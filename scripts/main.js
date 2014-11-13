39ac0a0facff62f0ad9e3f7bf175141b5109682c
Jess Scheuring <jscheuring@gmail.com>
2014-11-13 14:49:42 -0500

    build

--- scripts/main.js
+++ scripts/main.js
@@ -1453,7 +1453,7 @@ this["DanceCard"]["templates"]["orgs"]["org"]["newEvent"] = Handlebars.template(
       if (Parse.User.current()) {
         urlString = '/dancers/' + Parse.User.current().get('urlId');
       }
-      this.$el.append('<h1><a href=#"'+urlString+'">Dance Card</a></h1>');
+      this.$el.append('<h1><a href="#'+urlString+'">Dance Card</a></h1>');
       this.$el.append('<span class="tag-line">Do you want to dance?</span>');
       this.navView = new DanceCard.Views.Nav({
         $container: this.$el,
